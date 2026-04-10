1b81905ea8653c5f98e15d94c473b5fba404e82f
KADAMBALA YASWANTH <2400031811@kluniversity.in>
2026-04-10 10:21:44 +0530

    Delete #SKILL - 9 Global Exception Handling using @ControllerAdvice directory

--- #SKILL - 9 Global Exception Handling using @ControllerAdvice/src/main/java/com/klef/student/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package com.klef.student;
-
-public class InvalidInputException extends RuntimeException {
-
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
--- #SKILL - 9 Global Exception Handling using @ControllerAdvice/src/main/java/com/klef/student/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.klef.student;
-
-public class StudentNotFoundException extends RuntimeException {
-
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
